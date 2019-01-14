77d487728ad65e367f7fa1d97ee5805cb7e5c662
Simon Huang <simhuang92@gmail.com>
2019-01-14 01:49:45 -0500

    commit 3

--- learn.py
+++ learn.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-variable = 1
-desktop = "moses"
-=======
-variable = 1 
-desktop = "simon huang"
-desktop2 = "moses"
->>>>>>> 1f0899c34d6fda654f893dbb2d2e8925627cc2cf
+variable = "moses"
+temp = ['a', 'b', 'c']
